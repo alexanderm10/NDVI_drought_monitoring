7718bdd7f891d88ed04f8fcf815b1bc199c500e8
jharr101 <JHarr@mortonarb.org>
2025-03-31 10:40:23 -0500

    Update 07_boxplots_anovas.R

--- 07_boxplots_anovas.R
+++ 07_boxplots_anovas.R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(lubridate)
 library(cowplot)
 library(ggdist)
+library(multcompView)
 
 Sys.setenv(GOOGLE_DRIVE = "~/Google Drive/Shared drives/Urban Ecological Drought")
 google.drive <- Sys.getenv("GOOGLE_DRIVE")
@@ -60,24 +61,26 @@ grow_merge$percentage <- ""
 grow_merge <- grow_merge[!is.na(grow_merge$deviation),]
 grow_merge$severity <- factor(grow_merge$severity, levels=c("None", "D0", "D1", "D2", "D3"))
 
-png("~/Google Drive/Shared drives/Urban Ecological Drought/data/NDVI_drought_monitoring/figures/k=12_boxplots_redo/k=12_drought_category_deviations_box.png", height=6, width=12, units="in", res=320)
+#png("~/Google Drive/Shared drives/Urban Ecological Drought/data/NDVI_drought_monitoring/figures/k=12_boxplots_redo/k=12_drought_category_deviations_box.png", height=6, width=12, units="in", res=320)
 ggplot(data=grow_merge)+ #boxplots by drought category for each LC type
-  geom_boxplot(aes(x=percentage, y=deviation, fill=severity)) + xlab("> 50% coverage") +
+  geom_boxplot(aes(x=percentage, y=deviation, fill=severity)) + ylab("NDVI Anomaly") + xlab("> 50% Coverage") +
   scale_fill_manual(name="Category", values=c("None"="gray50", "D0"="yellow", "D1"="burlywood","D2"="darkorange", "D3"="red"))+
   facet_wrap(~type)+
   geom_hline(yintercept=0, linetype="dashed")+
-  ylim(-0.2,0.2) + theme_bw()
-dev.off()
+  ylim(-0.2,0.2) + theme_bw(10)
+ggsave("k=12_drought_category_deviations_box.png", path = pathShare, height=6, width=12, units="in", dpi = 320)
+#dev.off()
 
-png("~/Google Drive/Shared drives/Urban Ecological Drought/data/NDVI_drought_monitoring/figures/k=12_boxplots_redo/k=12_deviation_LCtype_redo.png", height=6, width=12, units="in", res=320)
+#png("~/Google Drive/Shared drives/Urban Ecological Drought/data/NDVI_drought_monitoring/figures/k=12_boxplots_redo/k=12_deviation_LCtype_redo.png", height=6, width=12, units="in", res=320)
 grow_merge$type <- factor(grow_merge$type, levels = c("crop", "forest", "grassland", "urban-open", "urban-low", "urban-medium", "urban-high"))
-ggplot(data=grow_merge) + xlab("> 50% coverage") + #boxplots by LC type for each drought category
+ggplot(data=grow_merge) + ylab("NDVI Anomaly") + xlab("> 50% Coverage") + #boxplots by LC type for each drought category
   geom_boxplot(aes(x=percentage, y=deviation, fill=type)) +
   scale_fill_manual(name="Type", values=c("crop"="darkorange3", "forest"="darkgreen", "grassland"="navajowhite1","urban-high"="darkred", "urban-medium"="red", "urban-low"="indianred","urban-open"="lightpink3"))+
   facet_wrap(~severity)+
   geom_hline(yintercept=0, linetype="dashed")+
-  ylim(-0.2,0.2) + theme_bw()
-dev.off()
+  ylim(-0.2,0.2) + theme_bw(10)
+ggsave("k=12_boxplots_redo/k=12_deviation_LCtype_redo.png", path = pathShare, height=6, width=12, units="in", dpi = 320)
+#dev.off()
 
 ######################
 #tukey matrix function https://rdrr.io/github/PhilippJanitza/rootdetectR/man/tukey_to_matrix.html
@@ -187,6 +190,9 @@ anova(anovurbop)
 summary(anovurbop)
 urbop <- aov(anovurbop)
 
+grow_merge_type <- group_by(grow_merge, type, severity)
+grow_merge_sum <- grow_merge_type %>% summarise(meandev=mean(deviation),sd=sd(deviation))
+
 ######################
 #Tukey tests by type
 ######################
@@ -210,32 +216,79 @@ plot(tukeyurbhi, las=1) + title(main='urban-high', col.main="red",line=0.6)
 dev.off()
 
 ######################
+#Tukey letter annotation
+######################
+cropletters <- multcompLetters4(crop, tukeycrop)
+cropletters <- as.data.frame.list(cropletters$severity)
+forestletters <- multcompLetters4(forest, tukeyforest)
+forestletters <- as.data.frame.list(forestletters$severity)
+grassletters <- multcompLetters4(grass, tukeygrass)
+grassletters <- as.data.frame.list(grassletters$severity)
+urbopletters <- multcompLetters4(urbop, tukeyurbop)
+urbopletters <- as.data.frame.list(urbopletters$severity)
+urblowletters <- multcompLetters4(urblow, tukeyurblow)
+urblowletters <- as.data.frame.list(urblowletters$severity)
+urbmedletters <- multcompLetters4(urbmed, tukeyurbmed)
+urbmedletters <- as.data.frame.list(urbmedletters$severity)
+urbhiletters <- multcompLetters4(urbhi, tukeyurbhi)
+urbhiletters <- as.data.frame.list(urbhiletters$severity)
+
+forestletters2 = c("a","b","b","ab","ab")
+letter_annotation <-rbind(cropletters$Letters, forestletters$Letters, grassletters$Letters, urbopletters$Letters, urblowletters$Letters, urbmedletters$Letters, urbhiletters$Letters)
+letter_list <- c(cropletters$Letters,forestletters2, grassletters$Letters, urbopletters$Letters, urblowletters$Letters, urbmedletters$Letters, urbhiletters$Letters)
+grow_merge_sum$Tukey <- letter_list
+
+ggplot()+
+  geom_boxplot(data=grow_merge, aes(x=type, y=deviation, fill=type)) +
+  ylab("NDVI Anomaly") + xlab("> 50% Coverage")+
+  geom_text(data=grow_merge_sum, aes(label=Tukey, x=type,y=meandev+sd),size = 3, vjust=-3, hjust =-1)+
+  scale_fill_manual(name="Type", values=c("crop"="darkorange3", "forest"="darkgreen", "grassland"="navajowhite1","urban-high"="darkred", "urban-medium"="red", "urban-low"="indianred","urban-open"="lightpink3"))+
+  facet_wrap(~severity)+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylim(-0.2,0.2) + theme_bw(10)+theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
+ggplot()+ #boxplots by drought category for each LC type
+  geom_boxplot(data=grow_merge,aes(x=severity, y=deviation, fill=severity)) + ylab("NDVI Anomaly") + xlab("> 50% Coverage") +
+  scale_fill_manual(name="Category", values=c("None"="gray50", "D0"="yellow", "D1"="burlywood","D2"="darkorange", "D3"="red"))+
+  geom_text(data=grow_merge_sum, aes(label=Tukey, x=severity,y=meandev+sd),size = 3, vjust=-2, hjust =-1)+
+  facet_wrap(~type)+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylim(-0.2,0.2) + theme_bw(10) +  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
+######################
 #anovas by drought category
 ######################
-anovnone <- lm(deviation~ type -1, data=grow_merge[grow_merge$severity=="None",])
+grow_merge2 <- grow_merge
+grow_merge2$type <- gsub("-", "", grow_merge2$type)
+
+anovnone <- lm(deviation~ type -1, data=grow_merge2[grow_merge2$severity=="None",])
 anova(anovnone)
 summary(anovnone)
 none <- aov(anovnone)
 
-anovd0 <- lm(deviation~ type -1, data=grow_merge[grow_merge$severity=="D0",])
+anovd0 <- lm(deviation~ type -1, data=grow_merge2[grow_merge2$severity=="D0",])
 anova(anovd0)
 summary(anovd0)
 d0 <- aov(anovd0)
 
-anovd1 <- lm(deviation~ type -1, data=grow_merge[grow_merge$severity=="D1",])
+anovd1 <- lm(deviation~ type -1, data=grow_merge2[grow_merge2$severity=="D1",])
 anova(anovd1)
 summary(anovd1)
 d1 <- aov(anovd1)
 
-anovd2 <- lm(deviation~ type -1, data=grow_merge[grow_merge$severity=="D2",])
+anovd2 <- lm(deviation~ type -1, data=grow_merge2[grow_merge2$severity=="D2",])
 anova(anovd2)
 summary(anovd2)
 d2 <- aov(anovd2)
 
-anovd3 <- lm(deviation~ type -1, data=grow_merge[grow_merge$severity=="D3",])
+anovd3 <- lm(deviation~ type -1, data=grow_merge2[grow_merge2$severity=="D3",])
 anova(anovd3)
 summary(anovd3)
 d3 <- aov(anovd3)
+
+grow_merge_sev <- group_by(grow_merge, severity, type)
+grow_merge_sum <- grow_merge_sev %>% summarise(meandev=mean(deviation),sd=sd(deviation))
+
 
 ######################
 #Tukey tests by drought category
@@ -245,6 +298,29 @@ tukeyd0 <- TukeyHSD(d0, conf.level = 0.95)
 tukeyd1 <- TukeyHSD(d1, conf.level = 0.95)
 tukeyd2 <- TukeyHSD(d2, conf.level = 0.95)
 tukeyd3 <- TukeyHSD(d3, conf.level = 0.95)
+
+noneletters <- multcompLetters4(none, tukeynone)
+noneletters <- as.data.frame.list(noneletters$type)
+d0letters <- multcompLetters4(d0, tukeyd0)
+d0letters <- as.data.frame.list(d0letters$type)
+d1letters <- multcompLetters4(d1, tukeyd1)
+d1letters <- as.data.frame.list(d1letters$type)
+d2letters <- multcompLetters4(d2, tukeyd2)
+d2letters <- as.data.frame.list(d2letters$type)
+d3letters <- multcompLetters4(d3, tukeyd3)
+d3letters <- as.data.frame.list(d3letters$type)
+
+sevletters <- c("a","a","a","a","a","a","a", "ab","ab","b","b","ab","ab","a","a","a","b","b","b","ab","a","a","ab","c","c","c","bc","ab","a","ab","c","c","c","c","b")
+grow_merge_sum$Tukey <- sevletters
+
+ggplot()+ #boxplots by drought category for each LC type
+  geom_boxplot(data=grow_merge,aes(x=severity, y=deviation, fill=severity)) + ylab("NDVI Anomaly") + xlab("> 50% Coverage") +
+  scale_fill_manual(name="Category", values=c("None"="gray50", "D0"="yellow", "D1"="burlywood","D2"="darkorange", "D3"="red"))+
+  geom_text(data=grow_merge_sum, aes(label=Tukey, x=severity,y=meandev+sd),size = 3, vjust=-2, hjust =-1)+
+  facet_wrap(~type)+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylim(-0.2,0.2) + theme_bw(10) +  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
 
 png("~/Google Drive/Shared drives/Urban Ecological Drought/data/NDVI_drought_monitoring/figures/k=12_boxplots_redo/k=12_tukey_LC.png",height=6, width=12, units="in", res=320)
 par(mfrow=c(2,3), col.main="black", mar=c(4,12,4,4))
@@ -296,3 +372,41 @@ ggplot(data=grow_merge, aes(x=deviation, y=type, fill=type))+
   geom_vline(xintercept=0,linetype="dashed")+
   scale_y_discrete(limits=rev)
 dev.off()
+
+######################
+#ANOVA (NDVIanom ~ LC x severity)
+######################
+grow_merge2 <- grow_merge
+grow_merge2$type <- gsub("-", "", grow_merge2$type)
+
+anovlc <- aov(deviation ~ type*severity, data=grow_merge2)
+summary(anovlc)
+
+grow_sum <- group_by(grow_merge, type, severity) %>% 
+  summarise(mean_anom=mean(deviation),sd=sd(deviation))
+
+tukeylc <- TukeyHSD(anovlc, conf.level=0.95)
+
+tukey_letters <- multcompLetters4(anovlc, tukeylc)
+tukey_letters <- as.data.frame.list(tukey_letters$`type:severity`)
+tukey_letters_ordered <- tukey_letters[c(6,11,14,16,22,5,9,15,20,10,3,18,26,30,33,1,19,27,31,35,2,17,24,29,34,4,12,23,28,32,7,8,13,21,25),]
+
+grow_sum$Tukey <- tukey_letters_ordered$Letters
+
+ggplot()+ #boxplots by drought category for each LC type
+  geom_boxplot(data=grow_merge,aes(x=severity, y=deviation, fill=severity)) + ylab("NDVI Anomaly") + xlab("> 50% Coverage") +
+  scale_fill_manual(name="Category", values=c("None"="gray50", "D0"="yellow", "D1"="burlywood","D2"="darkorange", "D3"="red"))+
+  geom_text(data=grow_sum, aes(label=Tukey, x=severity,y=mean_anom+sd),position=position_dodge2(0.75),size = 3, vjust=-3)+
+  facet_wrap(~type)+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylim(-0.2,0.2) + theme_bw(10) +  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+ggsave("2-way_anova_boxplot.png", path = "~/Downloads", height=6, width=12, units="in", dpi = 320)
+
+ggplot() + ylab("NDVI Anomaly") + xlab("> 50% Coverage") + #boxplots by LC type for each drought category
+  geom_boxplot(data=grow_merge, aes(x=type, y=deviation, fill=type)) +
+  geom_text(data=grow_sum, aes(label=Tukey, x=type,y=mean_anom+sd),position=position_dodge2(0.75),size = 3, vjust=-3)+
+  scale_fill_manual(name="Type", values=c("crop"="darkorange3", "forest"="darkgreen", "grassland"="navajowhite1","urban-high"="darkred", "urban-medium"="red", "urban-low"="indianred","urban-open"="lightpink3"))+
+  facet_wrap(~severity)+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylim(-0.2,0.2) + theme_bw(10) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+ggsave("2-way_anova_boxplot_alt.png", path = "~/Downloads", height=6, width=12, units="in", dpi = 320)
