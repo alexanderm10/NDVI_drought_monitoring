1e661ec10aa7925a683938d7151b9a8ec7e10e9a
Christy Rollinson <crollinson@gmail.com>
2025-05-29 08:49:41 -0500

    small changes from meeting 5/28

--- spatial_analysis/06_year_splines_yday_looped_.R
+++ spatial_analysis/06_year_splines_yday_looped_.R
@@ -11,7 +11,7 @@ path.google <- ("~/Google Drive/My Drive/")
 pathShare <- file.path(path.google, "../Shared drives/Urban Ecological Drought/data/spatial_NDVI_monitoring/yday_loop_yearly_splines")
 pathShare2 <- file.path(path.google, "../Shared drives/Urban Ecological Drought/data/spatial_NDVI_monitoring")
 
-source("../00_Calc_GAMM_posteriors_spatial_norms.R")
+source("00_Calc_GAMM_posteriors_spatial_norms.R")
 
 # load data ---------------------------------------------------------------
 
@@ -38,32 +38,66 @@ landsatYears$y <- as.numeric(landsatYears$y)
 landsatYears <- merge(landsatYears, landsatNorms, by = c("xy", "x", "y", "yday"))
 landsatYears <- landsatYears %>% rename(norm=mean, norm_lwr = lwr, norm_upr = upr)
 
-# yday years loop ---------------------------------------------------------
+modelStats <- data.frame(date=seq.Date(as.Date(paste0(min(landsatYears$year), "-01-01")), as.Date(paste0(max(landsatYears$year), "-12-31")), by="day"))
+modelStats$year <- lubridate::year(modelStats$date)
+modelStats$yday <- lubridate::yday(modelStats$date)
+modelStats[,c("R2", "Intercept", "NormCoef", "SplineP", "RMSE", "error")] <- NA
 
+# yday years loop ---------------------------------------------------------
+nPixels <- length(unique(landsatNorms$xy))
 for (yr in unique(landsatAll$year)){
-  
+  print(yr)
   yr_dates <- seq(as.Date(paste(yr,1,1,sep="-")), as.Date(paste(yr,12,31,sep="-")), by="day")
   
   yr_window <- subset(landsatAll, date >= as.Date(paste(yr-1,12,16, sep="-")) & date <= as.Date(paste(yr,12,31,sep="-")))
   yr_window <- merge(yr_window, landsatNorms, by = c("xy", "x", "y", "yday"))
   yr_window <- yr_window %>% rename(norm=mean)
   
-  for (day in 1:365){
+  pb <- txtProgressBar(min=1, max=365, style=3)
+  for (DAY in 1:365){
+    setTxtProgressBar(pb, DAY)
+    statsInd <- which(modelStats$year==yr & modelStats$yday==DAY)
     
-    ydays <- seq(yr_dates[day]-16, yr_dates[day], by="day")
+    ydays <- seq(yr_dates[DAY]-16, yr_dates[DAY], by="day")
     df_subset <- yr_window %>% filter(date %in% ydays)
     
+    # ggplot(data=df_subset[,]) +
+    #   coord_equal() +
+    #   facet_wrap(~yday) +
+    #   geom_tile(aes(x=x, y=y, fill=norm))
+    # 
+    # ggplot(data=df_subset[,]) +
+    #   coord_equal() +
+    #   facet_wrap(~yday) +
+    #   geom_tile(aes(x=x, y=y, fill=NDVIReprojected))
+    # 
+    # ggplot(data=df_subset[,]) +
+    #   coord_equal() +
+    #   facet_wrap(~yday + mission) +
+    #   geom_tile(aes(x=x, y=y, fill=NDVIReprojected-norm))
+    
+      if(length(unique(df_subset$xy[!is.na(df_subset$NDVIReprojected)]))<nPixels*0.33) next
     #if(length(which(!is.na(df_subset$NDVIReprojected)))<25) next
     
     gam_day <- gam(NDVIReprojected ~ norm + s(x,y), data=df_subset)
+    gamSummary <- summary(gam_day)
     
-    yr_day_Ind <- which(landsatYears$year==yr & landsatYears$yday==day)
+    modelStats$R2[statsInd] <- gamSummary$r.sq
+    # plot(df_subset$NDVIReprojected[!is.na(df_subset$NDVIReprojected)] ~ predict(gam_day))
+    modelStats[statsInd,c("Intercept", "NormCoef")] <- gamSummary$p.table[,"Estimate"]
+    modelStats$SplineP[statsInd] <- gamSummary$s.table[,"p-value"]
+    modelStats$error[statsInd] <- mean(residuals(gam_day))
+    # hist(residuals(gam_day))
+    modelStats$RMSE[statsInd] <- sqrt(mean(residuals(gam_day)^2))
+    
+    yr_day_Ind <- which(landsatYears$year==yr & landsatYears$yday==DAY)
     yr_day_post <- post.distns(model.gam=gam_day, newdata=landsatYears[yr_day_Ind,], vars=c("x","y"))
     landsatYears[yr_day_Ind,c("mean", "lwr", "upr")] <- yr_day_post[,c("mean", "lwr", "upr")]
     
-    saveRDS(gam_day, file.path(pathShare, paste0("yday=",day,"yr=", yr, "_year_spatial_gam")))
+    saveRDS(gam_day, file.path(pathShare, paste0("yday=",DAY,"yr=", yr, "_year_spatial_gam")))
 
-  }
-}
+  }# End day loop
+} # End year loop
+summary(modelStats)
 
 write.csv(landsatYears, file.path(pathShare2, "yday_spatial_loop_years.csv"), row.names=F)
