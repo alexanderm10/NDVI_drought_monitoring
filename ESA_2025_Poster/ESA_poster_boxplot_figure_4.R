b0cd5dd832e9ce52a54de55d2748d482beba7df1
jharr101 <JHarr@mortonarb.org>
2025-07-16 13:54:20 -0500

    minor tweaks to figure

--- ESA_2025_Poster/ESA_poster_boxplot_figure_4.R
+++ ESA_2025_Poster/ESA_poster_boxplot_figure_4.R
@@ -1,7 +1,5 @@
 library(ggplot2)
 library(tidyr)
-library(multcompView)
-library(stringr)
 
 Sys.setenv(GOOGLE_DRIVE = "~/Google Drive/Shared drives/Urban Ecological Drought")
 google.drive <- Sys.getenv("GOOGLE_DRIVE")
@@ -22,14 +20,14 @@ grow_sum$type <- factor(grow_sum$type, levels = c("crop", "forest", "grassland",
 # plot --------------------------------------------------------------------
 
 ggplot()+ #boxplots by drought category for each LC type
-  geom_boxplot(data=grow_merge[grow_merge$type!="grassland",],aes(x=severity, y=deviation, fill=severity)) + ylab("NDVI Anomaly") + xlab("> 50% Coverage") +
+  geom_boxplot(data=grow_merge[grow_merge$type!="grassland",],aes(x=severity, y=deviation, fill=severity)) + ylab("NDVI Anomaly") + xlab(NULL) +
   scale_fill_manual(name="Category", values=c("None"="gray50", "D0"="yellow", "D1"="burlywood","D2"="darkorange", "D3"="red"))+
   geom_text(data=grow_sum[grow_sum$type!="grassland",], aes(label=Tukey1, x=severity,y=mean_anom+sd),size = 3, vjust=-2, hjust =-1)+
   geom_text(data=grow_sum[grow_sum$type!="grassland",], aes(label=Tukey2, x=severity,y=mean_anom-sd),size = 3, vjust=2, hjust =-1)+
   facet_wrap(~type)+
   geom_hline(yintercept=0, linetype="dashed")+
-  labs(caption = "lowercase = drought category within land cover class \n uppercase = drought category across classes") +
-  ylim(-0.2,0.2) + theme_bw(15) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),plot.caption.position="plot",
+  labs(caption = "lowercase = effect of drought severity on NDVI anomalies within a given land cover class \nuppercase = effect of land cover type on NDVI anomalies within a given drought category") +
+  ylim(-0.2,0.2) + theme_bw(20) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),plot.caption.position="plot",
                                         plot.caption = element_text(hjust=0,vjust=0.5),plot.margin = margin(5.5,5.5,20,5.5,"pt"))
 
 ggsave("NDVI_anoms_boxplot_poster.png", path = pathShare3, height=6, width=12, units="in", dpi = 320)
